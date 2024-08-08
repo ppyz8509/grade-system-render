fc9fa7d71f76091595d7172f177234bfbb03eb6a
ppyz8509 <peenp1234@gmail.com>
2024-08-08 23:17:37 +0700

    update major and authcontreller

--- middlewares/authenticateToken.js
+++ middlewares/authenticateToken.js
@@ -9,6 +9,7 @@ const authenticateToken = (req, res, next) => {
 
   if (token == null) return res.sendStatus(401);
 
+  
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) return res.sendStatus(403);
     req.user = user;
