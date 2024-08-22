8cea10c80fba49465ef2fd84aba72cef9926fb79
ppyz8509 <peenp1234@gmail.com>
2024-08-23 00:49:50 +0700

    edit week7-8

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,23 +2,18 @@ const express = require("express");
 const router = express.Router();
 const {
   createUser,
-  ///getAllUser,
+  getAllUser,
   updateUser,
   deleteUser,
   getRole,
 } = require("../controllers/userController");
 const checkRole = require("../middlewares/checkRole");
 
-
-
-///admin zone
-router.post("/createUser",createUser);
-//router.get("/getAllUser", getAllUser);
-//router.get("/getRole/:role",getRole);
-//router.put("/updateUser/:id", updateUser);
-router.delete("/deleteUser/:id",  deleteUser);
+// ///admin zone
+// router.post("/createUser",createUser);
+// //router.get("/getAllUser", getAllUser);
+// //router.get("/getRole/:role",getRole);
+// //router.put("/updateUser/:id", updateUser);
+// router.delete("/deleteUser/:id",  deleteUser);
 
 module.exports = router;
-
-
-
