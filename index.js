2581be3d4e5523c1aae4da463a07767f515975ea
ppyz8509 <peenp1234@gmail.com>
2024-10-07 22:45:17 +0700

    edit json

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const connectDb = require('./models/prisma');
-
+const port = process.env.PORT || 5000;
 const authRouter = require("./routers/authRouter");
 const course_in_Router = require("./routers/course_in_Router");
 const majorRouter = require("./routers/majorRouter");
@@ -32,6 +32,10 @@ app.use(cors({
 }));
 
 app.use(express.json());
+app.get("/", (req, res) => {
+    res.send("connected to backend");    
+});
+
 
 // Combine Routers
 app.use("/api", authRouter);
@@ -48,7 +52,7 @@ app.use('/api', section);
 
 
 
-app.listen (3000,async () => {
+app.listen (port,async () => {
     await connectDb();
-    console.log("Server is running on port 3000");
+    console.log(`Server is running on port ${port}`);
 });
